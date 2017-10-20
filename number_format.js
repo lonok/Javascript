25c4461d1adcba60df1c0fe8b7065fc682f26efa
Leonardo Filipe <lonok@msn.com>
2017-10-20 16:21:09 -0200

    Update number_format.js

--- number_format.js
+++ number_format.js
@@ -1,12 +1,12 @@
-function number_format(string,decimals=2,decimal=',',thousands='.'){
+function number_format(string,decimals=2,decimal=',',thousands='.',pre='R$ ',pos=' $'){
   var numbers = string.toString().match(/\d+/g).join([]);
   numbers = numbers.padStart(decimals+1, "0");
   var splitNumbers = numbers.split("").reverse();
-  var mask;
+  var mask = '';
   splitNumbers.forEach(function(d,i){
     if (i == decimals) { mask = decimal + mask; }
     if (i>(decimals+1) && ((i-2)%(decimals+1))==0) { mask = thousands + mask; }
     mask = d + mask;
   });
-  return mask;
+  return pre + mask + pos;
 }
